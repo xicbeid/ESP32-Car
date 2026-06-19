821a80311e5dc9ccb7a6dd72b0945e050b22ee66
lss060909 <lss13543660811@gmail.com>
2026-06-19 19:49:01 +0800

    配置更改，针对我的电脑

--- build_esp.cmd
+++ build_esp.cmd
@@ -3,8 +3,8 @@ REM ============================================
 REM UART Project Build Script for ESP32-P4
 REM Run this from Command Prompt (cmd.exe)
 REM ============================================
-set IDF_PATH=F:\Espressif\frameworks\esp-idf-v5.4
-set IDF_TOOLS_PATH=F:\Espressif
-call F:\Espressif\frameworks\esp-idf-v5.4\export.bat
-cd /d F:\project_ESP32_p4\UART
+set IDF_PATH=E:\Esp32\frameworks\esp-idf-v5.4
+set IDF_TOOLS_PATH=E:\Esp32
+call E:\Esp32\frameworks\esp-idf-v5.4\export.bat
+cd /d E:\esp32-project\UART
 idf.py %*
